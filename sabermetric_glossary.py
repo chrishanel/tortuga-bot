56701c1f785ad0e8214851b596208d49d16d975e
Andrew <apkicklighter@gmail.com>
2022-02-22 02:12:20 -0600

    Fix import error, change to try_send_metric instead of try_send_message in sabermetric_glossary

--- sabermetric_glossary.py
+++ sabermetric_glossary.py
@@ -5,21 +5,42 @@ with open('messages.json', 'r') as message_file:
     message_data = json.load(message_file)
     glossary = message_data['sabermetric_glossary']
 
+
+"""
+Send the metric back to Discord
+
+"""
+
+
 async def send_metric(client, metric):
-    embed=discord.Embed(title=f"Sabermetric Glossary: {metric['name']}")
+    embed = discord.Embed(title=f"Sabermetric Glossary: {metric['name']}")
     embed.add_field(name="Definition", value=metric['definition'], inline=False)
     record = metric['record']
-    embed.add_field(name=f"Record {metric['name']}", value=f"{record['value']} - {record['holder']}, {record['year']}", inline=True)
+    embed.add_field(name=f"Record {metric['name']}", value=f"{record['value']} - {record['holder']}, {record['year']}",
+                    inline=True)
     await client.send(embed=embed)
+
+
+"""
+Send the metric via name
+
+"""
+
 
 async def send_metric_by_name(client, metric_name):
     await send_metric(client, glossary[metric_name])
 
-async def try_send_message(client, message):
+
+"""
+Try to send the metric back to Discord, give an error if it could not find the stat
+
+"""
+
+
+async def try_send_metric(client, message):
     target_metric = message.content.lower().split("!stat")[1].strip()
     if target_metric not in glossary:
         print(f"Error: couldn't find stat called {target_metric}")
         return
-    
+
     await send_metric_by_name(client, target_metric)
-    
